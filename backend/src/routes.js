ea137efe746100788735ce7be691d48ffb277cce
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-02-14 19:28:51 +0000

    folder: Controllers deleted

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,7 +6,7 @@
     Route Params ->  request.params ( identificar um recurso na alteração ou remoção)
     Body -> request.body (Dados para criação ou alteração de um registro)
  */ 
-// Express: micro-framework para as rotas da api
+//Express: micro-framework para as rotas da api
 const { Router } = require('express') //requisição de apenas a função 'Router' do express
 const routes = Router()
 const DevController = require('./controllers/DevController')
@@ -15,6 +15,6 @@ const SearchController = require('./controllers/SearchController')
 routes.get('/devs', DevController.index)
 routes.post('/devs',DevController.store)
 
-routes.get('/search', SearchController.index)
+routes.get('/search', DevController)
 
 module.exports = routes
