6f80eded9983a40522b2b452f2666941288c3a9d
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-03-18 01:00:28 +0000

    tratamento de erros e Insomnia

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -6,33 +6,45 @@ const parseStringAsArray = require('../utils/parseStringAsArray')
 module.exports = {
     async index(request, response){
         const devs = Dev.find()
-        return response.json(devs)
+        console.log(devs) //4DEBUG
+        return response.json({devs})
     },
 
     async store(request, response){ 
-        const { github_username, techs, latitude, longitude}= request.body 
+        const { github_username , techs, latitude, longitude}= request.body 
         let dev = await Dev.findOne({github_username})  // verificar a existencia do dev, p/ evitar duplicidade 
         if(!dev){
-            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+            const apiresponse = await axios.get(`https://api.github.com/users/${github_username}`)
+            const { name = login, avatar_url, bio } = apiresponse.data
+            console.log(name, avatar_url, bio, github_username)//DEBUG
         
-        const { name = login, avatar_url, bio } = apiresponse.data
-        console.log(name, avatar_url, bio, github_username)//DEBUG
-    
-        const techsArray = parseStringAsArray (techs)
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude,latitude]
-        }
-        dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs:techsArray,
-            location
-        }) 
+            const techsArray = parseStringAsArray (techs)
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude,latitude]
+            }
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs:techsArray,
+                location
+            }) 
         }
         return response.json(dev)
-    }
+    }/*,
+    async destroy( request,response){
+        //atualizar dados de um dev (como nome, avatar, bio, techs & localização)
+        const { github_username }= request.body
+        
+        try {
+            let dev = await Dev.findOneAndRemove({"github_username":{$eq : {github_username}}})  // verificar a existencia do dev, p/ evitar duplicidade 
+        } finally {
+            console.log(dev)
+            //return response.json(dev)
+        }
+    }*/
+    
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -14,9 +14,9 @@ const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.get('/devs', DevController.index);
-routes.post('/devs', DevController.store);
-
-routes.get('/search', SearchController.index);
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store)
+//routes.delete('devs', DevController.destroy)
+routes.get('/search', SearchController.index)
 
 module.exports = routes;
