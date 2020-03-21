5be37670f3a319d1425415577ff475cb0c7a82f2
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-03-21 11:07:42 +0000

    Start whit SideBar layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,42 @@
-import React, {useState} from 'react'
-import Lixo from './Teste'
+import React from 'react'
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
-  const [counter,setCounter] = useState(0)
-  function incrementa(){
-      setCounter(counter + 1)
-  }
   return (
-    <>
-      <h1>Contador : {counter}</h1>
-      <button onClick={incrementa}>Incrementar</button>
-    </>
+    <div id="app">
+        <aside>
+          <strong >Cadastrar</strong>
+          <form>
+            <div className="input-block">
+              <label htmlFor="github_username">Conta Github</label>
+              <input name="github_username" id="github_username" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="techs">Tecnologias</label>
+              <input name="techs" id="techs" required />
+            </div>
+
+            <div className="input-group">
+              <div className="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input name="latitude" id="latitude" required />
+              </div>
+              <div className="input-block">
+                <label htmlFor="longitude">Longitude</label>
+                <input name="longitude" id="longitude" required />
+              </div>
+            </div>
+            <button type="submit">Guardar</button>
+          </form>
+        </aside>
+        
+        <main>
+        </main>
+    </div>
     )
 }
 
