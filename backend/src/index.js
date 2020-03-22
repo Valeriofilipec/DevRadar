5c9882a0dda3da2b53a4e75c14909297ad45858c
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-03-22 15:58:45 +0000

    web project finished

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 const routes = require('./routes')
 const express = require('express')  // importando o express
-
+const cors = require('cors')
 const mongoose = require('mongoose') // importando o mongoose(MongoDB)
+const app = express()
 
 mongoose.connect('mongodb+srv://Valeriano:error404@devradar-cluster-f0t7h.mongodb.net/DevRadarDB?retryWrites=true&w=majority',{
   useNewUrlParser: true, 
@@ -10,8 +11,7 @@ mongoose.connect('mongodb+srv://Valeriano:error404@devradar-cluster-f0t7h.mongod
   useCreateIndex: true
 
 })
-
-const app = express()
+app.use(cors({}))//cors({origin:'http://localhost:3000'}) para restringir o acesso da aplicação 
 app.use(express.json()) //habilitar requisições do formato 'json' p/ qlqr rota c/ express
 app.use(routes)
 app.listen(3333)    //definindo a rota
