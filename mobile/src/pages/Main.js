99956b1d4946b67caed9bea365a519d6ac20276d
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-05-30 22:10:35 +0100

    ... Esquema de Navegação

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -2,14 +2,16 @@ import * as React from 'react';
 import {  Button,View, Text } from 'react-native';
 
 function Main({navigation}) {
-    return (
-      <View style={{ flex: 1,backgroundColor: '#A3CAA2', alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Valeriano Calológio</Text>
-        <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Profile')}
-      />
-      </View>
-    );
-  }
+  const navToProfile = () => navigation.navigate('Profile');
+
+  return (
+    <View style={{ flex: 1,backgroundColor: '#A3CAA2', alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Valeriano Calológio</Text>
+      <Button
+      title="Go to Profile"
+      onPress={navToProfile}
+    />
+    </View>
+  );
+}
 export default Main
--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 
 function Profile({navigation}) {
-    return (
-      <View style={{ flex: 1,backgroundColor: '#A3CAA2', alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Aqui é o Perfil GitHub</Text>
-      </View>
-    );
-  }
+
+  return (
+    <View style={{ flex: 1,backgroundColor: '#A3CAA2', alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Aqui é o Perfil GitHub</Text>
+    </View>
+  );
+}
 export default Profile
